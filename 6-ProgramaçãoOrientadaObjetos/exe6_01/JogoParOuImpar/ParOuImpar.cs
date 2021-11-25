b469753d013200ab9cfd0b6cb06d0923fea9e91b
Danilo Filitto <danilo.filitto@gmail.com>
2021-11-24 21:21:39 -0300

    Par ou Impar

---
+++ 6-ProgramaçãoOrientadaObjetos/exe6_01/JogoParOuImpar/ParOuImpar.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoParOuImpar
+{
+    public class ParOuImpar
+    {
+        
+        private string escolha;
+        //ou É PAR ou é IMPAR
+        public string Escolha
+        {
+            get { return escolha; }
+            set {
+                string texto = value.ToUpper();
+                if (texto=="PAR" || texto == "IMPAR")
+                {
+                    escolha = texto;
+                }
+                else
+                {
+                    escolha = "PAR";
+                }
+            }
+        }
+
+        public int Valor { get; set; }
+
+        public void GerarValor()
+        {
+            Random rnd = new Random();
+            Valor = rnd.Next(1,6);
+        }
+
+        public Boolean Ganhei(int ValorConcorrente)
+        {
+            Boolean retorno = false; //falando que não ganhei
+            int resultado = this.Valor + ValorConcorrente;
+            if (resultado % 2 == 0)
+            {
+                if (this.Escolha == "PAR")
+                {
+                    retorno = true;
+                }
+            }
+            else
+            {
+                if (this.Escolha == "IMPAR")
+                {
+                    retorno = true;
+                }
+            }
+            return retorno;
+        }
+
+    }
+}
---
+++ 6-ProgramaçãoOrientadaObjetos/exe6_01/JogoParOuImpar/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoParOuImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParOuImpar j1 = new ParOuImpar();
+            ParOuImpar pc = new ParOuImpar();
+            Console.WriteLine("jogo par ou impar");
+            Console.Write("Você quer ser par ou impar: ");
+            j1.Escolha = Console.ReadLine();
+            Console.Write("Informe um valor entre 1 e 5: ");
+            j1.Valor = Convert.ToInt32(Console.ReadLine());
+            //definir o pc
+            if (j1.Escolha == "PAR")
+            {
+                pc.Escolha = "IMPAR";
+            }
+            else
+            {
+                pc.Escolha = "PAR";
+            }
+            pc.GerarValor();
+            Console.WriteLine($"Eu escolhi {pc.Escolha} e informei o valor valor {pc.Valor}");
+            if (j1.Ganhei(pc.Valor) == true)
+            {
+                Console.WriteLine("Parabens!!!!! Você venceu.");
+            }
+            else
+            {
+                Console.WriteLine("Que Pena!!!!! Você perdeu.");
+            }
+            Console.ReadKey();
+            //definir se o resultado é par ou impar
+            //int resultado = j1.Valor + pc.Valor;
+            //if (resultado % 2 == 0)
+            //{
+            //    if (j1.Escolha == "PAR")
+            //    {
+            //        Console.WriteLine("Parabens!!!!! Você venceu.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Que Pena!!!!! Você perdeu.");
+            //    }
+            //}
+            //else
+            //{
+            //    if (j1.Escolha == "IMPAR")
+            //    {
+            //        Console.WriteLine("Parabens!!!!! Você venceu.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Que Pena!!!!! Você perdeu.");
+            //    }
+            //}
+
+        }
+    }
+}
